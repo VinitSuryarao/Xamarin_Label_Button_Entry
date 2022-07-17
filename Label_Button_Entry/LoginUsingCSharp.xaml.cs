5ce1e684ffe68f162913cc7ed067432d33642e9f
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-07-17 11:03:35 +0530

    First Commit

---
+++ Label_Button_Entry/LoginUsingCSharp.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Label_Button_Entry
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUsingCSharp 
+    {
+        public LoginUsingCSharp()
+        {
+            //InitializeComponent();
+
+            this.BackgroundColor = Color.Azure;
+            var lblUserName = new Label { Text = "UserName", TextColor = Color.Black };
+            var entryUserName = new Entry { Placeholder = "Enter User Name" };
+            var lblPassword = new Label { Text = "Password", TextColor = Color.Black };
+            var entryPassword = new Entry { Placeholder = "Enter Password", IsPassword = true };
+            var btnLogin = new Button { Text = "Login", BackgroundColor = Color.Yellow };
+            btnLogin.Clicked += LoginEvent;
+
+            this.Content = new StackLayout
+            {
+                Padding = new Thickness(20),
+                Margin = new Thickness(0, 80, 0, 0),
+                Children =
+                {
+                    lblUserName, entryUserName, lblPassword, entryPassword, btnLogin
+                }
+            };
+        }
+
+        private void LoginEvent(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull", "Ok");
+        }
+    }
+}
---
+++ Label_Button_Entry/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Label_Button_Entry
+{
+    public partial class MainPage 
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "Login Sucessfull","Ok");
+        }
+    }
+}
